01f757fdc897f095f7f92f54630cb8b9ed35f0c7
Andrew Korchevskiy <37622720+Salamandero@users.noreply.github.com>
2022-10-25 10:26:05 +0300

    done task

---
+++ src/fetchCountries.js
@@ -0,0 +1,15 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+export default function fetchCountries(name) {
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,name.official,capital,population,flags,languages`
+  ).then(response => {
+      if (!response.ok) {
+        Notify.failure('Oops, there is no country with that name!');
+      }
+      return response.json();
+    })
+    .catch(error => {
+      console.log('error in catch', error);
+    });
+}
---
+++ src/index.js
@@ -0,0 +1,69 @@
+import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import fetchCountries from './fetchCountries.js';
+
+const searchInput = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+var debounce = require('lodash.debounce');
+const DEBOUNCE_DELAY = 300;
+
+searchInput.addEventListener('input', debounce(searchCountry, DEBOUNCE_DELAY));
+
+function searchCountry(e) {
+  e.preventDefault();
+  let countryName = `${searchInput.value}`.trim();
+  if (searchInput.value === '') {
+    countryList.innerHTML = '';
+    countryInfo.innerHTML = '';
+  } else {
+    fetchCountries(countryName).then(data => {
+      if (data.length > 10) {
+        Notify.info('Too many matches found. Please enter a more specific name.');
+        countryList.innerHTML = '';
+        return;
+        } if (data.length > 1) {
+          createCountryListMarkup(data);
+          countryInfo.innerHTML = '';
+        } else {
+          countryList.innerHTML = '';
+          createInfoMarkup(...data);
+        }
+      })
+      .catch(error => {
+        console.log('error in catch', error);
+      });
+  }
+}
+
+function createListMarkup(array) {
+  return `<li class="country-list__item">
+    <img class="country-list__flags" src="${array.flags.svg}" alt="${array.name.official}" width=30 height=20>
+    <p class="country-list__name">${array.name.official}</p>
+  </li>`;
+}
+function createCountryListMarkup(arrays) {
+  const result = arrays.reduce((acc, array) => acc + createListMarkup(array), '');
+  return countryList.innerHTML = result;
+}
+
+function createInfoMarkup(array) {
+  return countryInfo.innerHTML = `<h2 class="country-info__title">
+      <img class="country-info__flag" src="${array.flags.svg}" alt="${
+    array.name.official
+  }" width=50 height=30 />
+      ${array.name.official}
+    </h2>
+    <ul class="country-info__list">
+      <li class="country-info__item"><span class="country-info__text">Capital:</span> ${
+        array.capital
+      }</></li>
+      <li class="country-info__item"><span class="country-info__text">Population:</span> ${
+        array.population
+      }</></li>
+      <li class="country-info__item"><span class="country-info__text">Languages:</span> ${Object.values(
+        array.languages
+      )}</></li>
+    </ul>`;
+}
